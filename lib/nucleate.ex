24b02a41734a7c5d63aff39e466294585da7408e
Brian Ewell <brian@brianewell.com>
2025-06-28 08:16:13 +0000

    Initialized mix mix new nucleate

---
+++ lib/nucleate.ex
@@ -0,0 +1,18 @@
+defmodule Nucleate do
+  @moduledoc """
+  Documentation for `Nucleate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nucleate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
